511d0ae18d8b9de4e1a52b7ef7fbb68eb26b2bf1
Getu Gebre <getu.gebre.gg@gmail.com>
2022-04-25 23:50:47 -0500

    update css

--- mvc/view.js
+++ mvc/view.js
@@ -20,7 +20,7 @@ export const View = (() => {
             tmp += `
                 <div class="todo_container">
                     <button ${!todo.isCompleted ? 'hidden' : 'visible'} class="completedbtn ${todo.id}">
-                        <i> <svg focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="ArrowBackIcon" aria-label="fontSize small"><path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"></path></svg></i>
+                        <svg focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="ArrowBackIcon" aria-label="fontSize small"><path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"></path></svg>
                     </button>
                     <div>
                         ${ !todo.editable 
